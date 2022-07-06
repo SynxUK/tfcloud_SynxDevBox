b6334285eec17950ab8db1d14d7b6ffce85a9c05
StuartD <stuart.dryden75@gmail.com>
2022-07-06 10:40:20 +0100

    location via param

--- rg.tf
+++ rg.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.AppName
-  location = "uk-south"
+  location = var.Location
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "AppName" {
   type = string
   description = "App name used in all naming"
 }
+
+variable "Location" {
+  type = string
+  description = "Singular Azure location for the deployment"
+}
